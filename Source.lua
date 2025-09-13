55aee951f24b35ee7d53d4ae10bd6b18053fae95
MrxtDJPhonk <santilindo55@gmail.com>
2025-09-13 17:16:03 +0100

    Update and rename README.md to Source.lua

---
+++ Source.lua
@@ -0,0 +1,7 @@
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+    Title = "DJ Phonk";
+    Text = "BY DJ Phonk";
+    Icon = "rbxthumb://type=Asset&id=106808374231896&w=150&h=150";
+    Duration = 5;
+})
+Return loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/nightsintheforest.lua"))()
